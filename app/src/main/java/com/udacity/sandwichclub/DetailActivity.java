14b3f746402e64484c6d38f4b1806c2d4682a0aa
KavinRaju <rajukavin25@gmail.com>
2018-06-02 11:07:25 +0530

    First commit Done with all the required changes.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,32 +1,49 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.Objects;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    ImageView ingredientsIv;
+    TextView textView_orgin;
+    TextView textView_also_known_as;
+    TextView textView_ingredients;
+    TextView textView_description;
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
+        assert intent != null;
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -43,12 +60,15 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
 
-        setTitle(sandwich.getMainName());
+
+        toolbar.setTitle(sandwich.getMainName());
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 
     private void closeOnError() {
@@ -56,7 +76,45 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        ingredientsIv = findViewById(R.id.image_iv);
+        textView_orgin = findViewById(R.id.origin_tv);
+        textView_also_known_as = findViewById(R.id.also_known_tv);
+        textView_ingredients = findViewById(R.id.ingredients_tv);
+        textView_description = findViewById(R.id.description_tv);
+
+        textView_orgin.setText(sandwich.getPlaceOfOrigin());
+        textView_description.setText(sandwich.getDescription());
+
+
+        if(sandwich.getAlsoKnownAs().size() > 0) {
+
+            for (int i = 0; i < sandwich.getAlsoKnownAs().size();i++){
+                textView_also_known_as.append(sandwich.getAlsoKnownAs().get(i) + "\n");
+            }
+
+        }
+        else {
+            textView_also_known_as.setText("...");
+        }
+
+        if (sandwich.getIngredients().size() > 0) {
+
+            for (int i = 0; i < sandwich.getIngredients().size();i++){
+                textView_ingredients.append(sandwich.getIngredients().get(i)  + "\n");
+            }
+
+        }
+        else {
+            textView_ingredients.setText("...");
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        finish();
+        return true;
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,58 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich sandwich = new Sandwich();
+
+
+        try {
+            JSONObject jsonObject_main = new JSONObject(json);
+
+            JSONObject jsonObject_name = jsonObject_main.getJSONObject("name");
+            sandwich.setMainName(jsonObject_name.getString("mainName"));  // mainName
+
+            List<String> alsoKnownAs = new ArrayList<>();
+            JSONArray jsonArray_alsoKnownAs = jsonObject_name.getJSONArray("alsoKnownAs");
+            if (jsonArray_alsoKnownAs != null && jsonArray_alsoKnownAs.length() > 0) {
+                for (int i = 0; i < jsonArray_alsoKnownAs.length() - 1 ; i++) {
+
+                        alsoKnownAs.add(jsonArray_alsoKnownAs.getString(i));
+
+                }
+            }
+            sandwich.setAlsoKnownAs(alsoKnownAs);  // alsoKnownAs
+
+            sandwich.setPlaceOfOrigin(jsonObject_main.getString("placeOfOrigin")); // Place of orgin
+            sandwich.setDescription(jsonObject_main.getString("description"));    // Description
+            sandwich.setImage(jsonObject_main.getString("image"));   // Image URL
+
+            List<String> ingredients  = new ArrayList<>();
+            JSONArray jsonArray_ingredients = jsonObject_main.getJSONArray("ingredients");
+            if (jsonArray_ingredients != null && jsonArray_ingredients.length() > 0) {
+
+                for (int i = 0; i < jsonArray_ingredients.length() - 1; i++) {
+
+                    ingredients.add(jsonArray_ingredients.getString(i));
+                }
+            }
+            sandwich.setIngredients(ingredients);   // Ingredients
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return sandwich;
     }
 }
